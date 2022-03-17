7a20a8ac87c81fa4551bc534713807e8bf2b0ca0
joaoAugustoParaty <joaodrow@gmail.com>
2022-03-17 01:17:27 -0300

    Criando o GridItem

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -7,10 +7,10 @@ export type level ={
 
 }
 export const levels: level[] = [
- {title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.5]},
- {title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
- {title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30]},
- {title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99]},
+ {title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.5] },
+ {title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9] },
+ {title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30] },
+ {title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99] },
 
 ];
 
